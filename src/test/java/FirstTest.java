bbddb43c2c4631fbeca6ab21949f5f85e628044f
Amingob <amingo.mashigo@yahoo.com>
2022-09-06 13:08:45 +0200

    Jessica second test

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,12 @@
+import basetest.BaseTest;
+import org.testng.annotations.Test;
+
+public class FirstTest extends BaseTest {
+
+    @Test
+    public void testDrivers(){
+        setUp();
+        tearDown();
+    }
+
+}
